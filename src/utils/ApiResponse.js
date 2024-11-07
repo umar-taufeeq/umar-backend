bc6d939f86ed69659daec1fe54dfec16731c6a58
umar-taufeeq <umarckp111@gmail.com>
2024-11-07 11:28:44 +0530

    utils code discussion

---
+++ src/utils/ApiResponse.js
@@ -0,0 +1,8 @@
+ class ApiResponse {
+    consructor(statusCode,data,message = success){
+        this.statusCode =statusCode
+        this.data =data
+        this.message = message
+        this.success = statusCode < 400
+    }
+ }
