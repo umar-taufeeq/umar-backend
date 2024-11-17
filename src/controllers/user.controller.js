f16d109c85a5161493a3fb1d0d7f72782f4575cd
umar-taufeeq <umarckp111@gmail.com>
2024-11-17 20:33:51 +0530

    fix all register controller bugs

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -17,7 +17,7 @@ const registerUser = asyncHandler (async (req ,res) =>{
 //    return  response
     
    const {fullName,username,email,password}=req.body
-   console.log("email: ",email);
+   //console.log("email: ",email);
    
 
    if (
@@ -26,14 +26,19 @@ const registerUser = asyncHandler (async (req ,res) =>{
    {
     throw new ApiError(400,"all feilds are required")
    }
-   const existedUser = User.findOne({
+   const existedUser = await User.findOne({
     $or:[{username},{email}]
    })
    if (existedUser){
     throw new ApiError(409,"user with email or username already exists")
    }
    const avatarLocalPath = req.files?.avatar[0]?.path;
-   const coverImageLocalPath=req.files?.coverImage[0]?.path;
+   //const coverImageLocalPath=req.files?.coverImage[0]?.path;
+
+   let coverImageLocalPath;
+   if (req.files&&Array.isArray(req.files.coverImage)&&req.files.coverImage.length>0){
+    coverImageLocalPath =req.files.coverImage[0].path
+   }
 
    if(!avatarLocalPath){
     throw new ApiError (400,"avatar file is required")
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -10,19 +10,20 @@ import fs from "fs"
     });
 
 
-    const uploadOnCloudinary = async(localFilPath)=>{
+    const uploadOnCloudinary = async(localFilePath)=>{
         try {
-            if(!localFilPath)return null
+            if(!localFilePath)return null
             //upload the file on cloudinary
-            const response =await cloudinary.uploader.upload(localFilPath,{
+            const response =await cloudinary.uploader.upload(localFilePath,{
                 resource_type:"auto"
             })
             //file has been succesfully uploaded
-            console.log("file is uploaded on cloudinary",response.url);
+            // console.log("file is uploaded on cloudinary",response.url);
+            fs.unlinkSync(localFilePath) 
             return response;
-            
+          
         } catch (error) {
-            fs.unlinkSync(localFilPath)//remove the locally saved temporary file as then upload operation got failed
+            fs.unlinkSync(localFilePath)//remove the locally saved temporary file as then upload operation got failed
             return null;
 
         }
